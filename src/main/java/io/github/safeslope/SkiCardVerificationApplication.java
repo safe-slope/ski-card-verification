41f7e7241fda74a39fd0ca46cf6ebd6d82eb3e5a
nicknick75 <nika.kralj27@gmail.com>
2026-01-10 15:51:22 +0100

    Pom.xml + application class

---
+++ src/main/java/io/github/safeslope/SkiCardVerificationApplication.java
@@ -0,0 +1,12 @@
+package io.github.safeslope.skicard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiCardVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkiCardVerificationApplication.class, args);
+    }
+}
